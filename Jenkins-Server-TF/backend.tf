b4b2d48c7e68fe907051880a6e3164725b4d2f47
jenish-chauhan <chauhanjenish@gmail.com>
2026-07-26 15:04:43 +0530

    update jenkins

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   # Store state remotely so the Jenkins infrastructure can be managed
   # consistently across runs and collaborators.
   backend "s3" {
-    bucket       = "backend-store-terraform-stat"
+    bucket       = "jerry961"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
